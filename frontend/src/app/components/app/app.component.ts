f80ed225aff5c66f1635090f1b57ff290cbbe78a
Leonid Veretennikov <megalok10@gmail.com>
2019-01-20 01:48:56 +0300

    removed unused methods in appComponent

--- frontend/src/app/components/app/app.component.ts
+++ frontend/src/app/components/app/app.component.ts
@@ -1,26 +1,8 @@
 import { Component } from '@angular/core';
-import { Graph } from '../../types/Graph';
-import { Subject } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-    public graphData: Subject<Graph>;
-    public schedule: Subject<string[][]>;
-
-    constructor() {
-        this.graphData = new Subject<Graph>();
-        this.schedule = new Subject<string[][]>();
-    }
-
-    getGraphData(graph: Graph) {
-        this.graphData.next(graph);
-    }
-
-    getSchedule(schedule: string[][]) {
-        this.schedule.next(schedule);
-    }
-}
+export class AppComponent {}
