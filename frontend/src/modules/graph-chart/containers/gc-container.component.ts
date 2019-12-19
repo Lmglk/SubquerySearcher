92bf640474110a5f1b84d80159e5541c1c15d6e9
Leonid Veretennikov <megalok10@gmail.com>
2019-12-20 01:42:23 +0300

    Remove unless components

--- frontend/src/modules/graph-chart/containers/gc-container.component.ts
+++ frontend/src/modules/graph-chart/containers/gc-container.component.ts
@@ -3,39 +3,36 @@ import {
     Component,
     ElementRef,
     HostListener,
+    OnDestroy,
     ViewChild,
 } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { combineLatest, Subscription } from 'rxjs';
+import { filter, tap } from 'rxjs/operators';
 import { selectGraph } from '../selectors/selectGraph';
 import { Graph } from '../../app/interfaces/Graph';
 import { Group } from '../../app/interfaces/Group';
 import { AppState } from '../../app/interfaces/AppState';
 import { selectGroups } from '../../app/selectors/selectGroups';
 import { getMetricWidth } from '../../metrics/selectors/getMetricWidth';
+import { GraphChartNode } from '../interfaces/GraphChartNode';
+import { GraphChartEdge } from '../interfaces/GraphChartEdge';
+import * as d3 from 'd3';
 
 @Component({
     selector: 'ssw-gc-container',
     template: `
         <ssw-block class="block">
             <div class="content" #content>
-                <ssw-free-placement-container
-                    *ngIf="(groups$ | async).length === 0"
-                    [graph]="graph$ | async"
-                    [nodeRadius]="nodeRadius"
+                <ssw-gc-canvas
+                    [nodes]="scaledNodes"
+                    [edges]="scaledEdges"
                     [height]="height"
                     [width]="width"
-                ></ssw-free-placement-container>
-
-                <ssw-gc-sequence-placement-container
-                    *ngIf="(groups$ | async).length !== 0"
-                    [graph]="graph$ | async"
                     [nodeRadius]="nodeRadius"
-                    [height]="height"
-                    [width]="width"
-                    [groups]="groups$ | async"
-                    [maxGroupSize]="maxGroupSize$ | async"
-                ></ssw-gc-sequence-placement-container>
+                    [verticalDragging]="true"
+                    [horizontalDragging]="false"
+                ></ssw-gc-canvas>
             </div>
         </ssw-block>
     `,
@@ -52,26 +49,48 @@ import { getMetricWidth } from '../../metrics/selectors/getMetricWidth';
         `,
     ],
 })
-export class GcContainerComponent implements AfterViewInit {
-    @ViewChild('content', { static: false }) element: ElementRef;
-
+export class GcContainerComponent implements AfterViewInit, OnDestroy {
     public readonly nodeRadius = 15;
+
+    @ViewChild('content', { static: false }) element: ElementRef;
 
     public height: number;
     public width: number;
+    public graph: Graph;
+    public scaledNodes: GraphChartNode[] = [];
+    public scaledEdges: GraphChartEdge[] = [];
 
-    public graph$: Observable<Graph>;
-    public groups$: Observable<Group[]>;
-    public maxGroupSize$: Observable<number>;
+    private scaleX: (value: number) => number;
+    private scaleY: (value: number) => number;
+    private storeSubscription: Subscription;
 
-    constructor(private readonly store: Store<AppState>) {
-        this.graph$ = this.store.pipe(select(selectGraph));
-        this.groups$ = this.store.pipe(select(selectGroups));
-        this.maxGroupSize$ = this.store.pipe(select(getMetricWidth));
-    }
+    constructor(private readonly store: Store<AppState>) {}
 
     public ngAfterViewInit(): void {
         setTimeout(() => this.getContentSize());
+
+        this.storeSubscription = combineLatest(
+            this.store.pipe(select(selectGraph)),
+            this.store.pipe(select(selectGroups)),
+            this.store.pipe(select(getMetricWidth))
+        )
+            .pipe(
+                filter(
+                    ([graph, groups, maxGroupSize]) =>
+                        Boolean(groups.length) && maxGroupSize > 0
+                ),
+                tap(([graph, groups, maxGroupSize]) => {
+                    this.graph = graph;
+
+                    this.calculateScalesForSchedule(
+                        groups.length,
+                        maxGroupSize
+                    );
+                    this.calculateNodeTicksForSchedule(groups, maxGroupSize);
+                    this.calculateEdgeTicks();
+                })
+            )
+            .subscribe();
     }
 
     @HostListener('window:resize')
@@ -82,5 +101,65 @@ export class GcContainerComponent implements AfterViewInit {
     private getContentSize() {
         this.height = this.element.nativeElement.clientHeight;
         this.width = this.element.nativeElement.clientWidth;
+    }
+
+    private calculateScalesForSchedule(
+        groupCount: number,
+        maxGroupSize: number
+    ) {
+        this.scaleX = d3
+            .scaleLinear()
+            .domain([0, groupCount - 1])
+            .range([this.nodeRadius, this.width - this.nodeRadius * 2]);
+        this.scaleY = d3
+            .scaleLinear()
+            .domain([0, maxGroupSize - 1])
+            .range([this.nodeRadius, this.height - this.nodeRadius * 2]);
+    }
+
+    private calculateNodeTicksForSchedule(
+        groups: Group[],
+        maxGroupSize: number
+    ) {
+        this.scaledNodes = [];
+        groups.forEach((group, groupIndex) => {
+            group.sequences.forEach((sequence, sequenceIndex) => {
+                sequence.nodes.forEach((nodeId, nodeIndex) => {
+                    const node = this.graph.nodes.filter(
+                        item => item.id === nodeId
+                    )[0];
+
+                    this.scaledNodes = [
+                        ...this.scaledNodes,
+                        {
+                            ...node,
+                            x:
+                                this.scaleX(groupIndex) +
+                                nodeIndex *
+                                    (this.nodeRadius * 2 + this.nodeRadius),
+                            y: this.scaleY(maxGroupSize - sequenceIndex - 1),
+                        },
+                    ];
+                });
+            });
+        });
+    }
+
+    private calculateEdgeTicks() {
+        this.scaledEdges = this.graph.edges.map(edge => {
+            return {
+                id: edge.id,
+                source: this.scaledNodes.find(
+                    node => node.id === edge.sourceId
+                ),
+                target: this.scaledNodes.find(
+                    node => node.id === edge.targetId
+                ),
+            };
+        });
+    }
+
+    public ngOnDestroy(): void {
+        this.storeSubscription.unsubscribe();
     }
 }
--- frontend/src/modules/graph-chart/graph-chart.module.ts
+++ frontend/src/modules/graph-chart/graph-chart.module.ts
@@ -4,8 +4,6 @@ import { GcNodeComponent } from './components/gc-node/gc-node.component';
 import { GcCanvasComponent } from './components/gc-canvas/gc-canvas.component';
 import { GcContainerComponent } from './containers/gc-container.component';
 import { GcEdgeComponent } from './components/gc-edge/gc-edge.component';
-import { GcFreePlacementContainerComponent } from './containers/gc-free-placement-container.component';
-import { GcSequencePlacementContainerComponent } from './containers/gc-sequence-placement-container.component';
 import { ControlsModule } from '@ssw/controls';
 
 @NgModule({
@@ -14,8 +12,6 @@ import { ControlsModule } from '@ssw/controls';
         GcCanvasComponent,
         GcNodeComponent,
         GcEdgeComponent,
-        GcFreePlacementContainerComponent,
-        GcSequencePlacementContainerComponent,
     ],
     imports: [CommonModule, ControlsModule],
     exports: [GcContainerComponent],
